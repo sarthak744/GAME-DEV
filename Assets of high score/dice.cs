e86a89d78cbf51069997739278d4955289a34618
sarthak744 <56968440+sarthak744@users.noreply.github.com>
2020-10-10 17:32:31 +0530

    Add files via upload

---
+++ Assets of high score/dice.cs
@@ -0,0 +1,33 @@
+﻿
+using System.CodeDom.Compiler;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dice : MonoBehaviour
+{
+    public Text score;
+    public Text hs;
+    void Start()
+    {
+        hs.text = PlayerPrefs.GetInt("highscore",0).ToString();
+    }
+    public void rolldice()
+    {
+        int temp = Random.Range(1, 7);
+        score.text = temp.ToString();
+        if(temp>PlayerPrefs.GetInt("highscore",0))
+        {
+            PlayerPrefs.SetInt("highscore", temp);
+            hs.text = temp.ToString();
+        }
+       
+    }
+
+    public void reset()
+    {
+        PlayerPrefs.DeleteKey("highscore");
+        hs.text = "0";
+
+    }
+}
