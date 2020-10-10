c7b5d8cdb5da3e66418cbf946c39cb4082734582
sarthak744 <56968440+sarthak744@users.noreply.github.com>
2020-10-10 17:36:44 +0530

    Add files via upload

---
+++ Assets of navmesh/scripts/playermov.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class playermov : MonoBehaviour
+{
+
+    public Camera cam;
+    public NavMeshAgent ag;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                //move our agent
+                ag.SetDestination(hit.point);
+            }
+        }
+        
+    }
+}
